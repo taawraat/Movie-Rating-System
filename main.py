1bbb4a85bb87a0dca2fbf90121b218cfc0640e86
Zahidul Islam Tawrat <tawraat0187@gmail.com>
2024-04-03 22:04:23 +0600

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.include_router(rating_router)
 
 
 def start():
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
 
 if __name__ == "__main__":
     start()
